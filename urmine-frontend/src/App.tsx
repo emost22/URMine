2c17f8af825d427166946f09b3ff4ff17e5fe80a
ohwoo-kwon <kwonow1@gmail.com>
2022-04-26 22:30:07 +0900

    Feat: Add profile route

--- urmine-frontend/src/App.tsx
+++ urmine-frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Routes/Home";
 import Login from "./Routes/Login";
+import Profile from "./Routes/Profile";
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/user/:id" element={<Profile />} />
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<Home />} />
       </Routes>
