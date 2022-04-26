2c646b8a44235d81cf35d44c5f14c811c84eadca
emost22 <emoney96@naver.com>
2022-04-26 20:13:47 +0900

    Feat: Backend init

---
+++ urmine-backend/src/test/java/com/urmine/UrmineBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.urmine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrmineBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
