6a2b7e1cdcb5be4e029a95078af216bc433c2ca8
ohwoo-kwon <kwonow1@gmail.com>
2022-04-26 22:28:35 +0900

    Feat: Add Login Route

--- urmine-frontend/src/App.tsx
+++ urmine-frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Routes/Home";
+import Login from "./Routes/Login";
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/login" element={<Login />} />
         <Route path="/" element={<Home />} />
       </Routes>
     </Router>
