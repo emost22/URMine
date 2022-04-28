8bf8e26ad3e1b0de27f4733efa602a97e6cf8ae9
ohwoo-kwon <kwonow1@gmail.com>
2022-04-28 22:47:21 +0900

    Design: Login

--- urmine-frontend/src/Routes/Login.tsx
+++ urmine-frontend/src/Routes/Login.tsx
@@ -1,5 +1,24 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  background-color: burlywood;
+  flex: 1;
+  position: relative;
+`;
+
+const LoginImg = styled.img`
+  position: relative;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+
 function Login() {
-  return <div>Login</div>;
+  return (
+    <Container>
+      <LoginImg src="/images/kakao_login_large_wide.png" alt="login" />
+    </Container>
+  );
 }
 
 export default Login;
