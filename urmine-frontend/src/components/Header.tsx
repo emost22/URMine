aaa8d6e63e9d0eba14f0fece57a5c183d3da9be5
ohwoo-kwon <kwonow1@gmail.com>
2022-04-28 22:29:15 +0900

    Feat: Add link in icons

--- urmine-frontend/src/components/Header.tsx
+++ urmine-frontend/src/components/Header.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const Head = styled.header`
@@ -18,12 +19,15 @@ const Icons = styled.div`
   height: 55px;
 `;
 
-const IconBox = styled.div`
+const StyledLink = styled(Link)`
   display: flex;
   flex-direction: column;
-  padding-left: 50px;
+  margin-left: 50px;
   align-items: center;
   justify-content: space-between;
+  cursor: pointer;
+  text-decoration: none;
+  color: black;
 `;
 
 const Icon = styled.img`
@@ -36,54 +40,38 @@ const IconDesc = styled.span`
 `;
 
 function Header() {
-  const isLogin = true;
+  const isLogin = false;
   return (
     <Head>
-      <Logo
-        src="https://s3.us-west-2.amazonaws.com/secure.notion-static.com/1091081b-d89e-4a4c-a0f7-a106f0bae663/Untitled.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20220427%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220427T123949Z&X-Amz-Expires=86400&X-Amz-Signature=b11afd03adb67215e3c10550ea3ff405ac88d78f290697a545bdae39da59ff7b&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22Untitled.png%22&x-id=GetObject"
-        alt="Logo"
-      />
+      <Link to={"/"}>
+        <Logo src="/images/logo.png" alt="Logo" />
+      </Link>
       <Icons>
         {isLogin ? (
           <>
-            <IconBox>
-              <Icon
-                src="https://s3.us-west-2.amazonaws.com/secure.notion-static.com/329035de-a642-4cfa-93d3-6b815f644493/free-icon-pokeball-361998.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20220427%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220427T124807Z&X-Amz-Expires=86400&X-Amz-Signature=37e2d1550b54f8a256718488ce108b0a4057a03d76cdd87da63e31ffbf8773a7&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22free-icon-pokeball-361998.png%22&x-id=GetObject"
-                alt="pokedex"
-              />
+            <StyledLink to={"/pokedex"}>
+              <Icon src="/images/monsterball.png" alt="pokedex" />
               <IconDesc>내 도감</IconDesc>
-            </IconBox>
-            <IconBox>
-              <Icon
-                src="https://s3.us-west-2.amazonaws.com/secure.notion-static.com/52d90fa2-fe58-4be2-b64f-1d172205547c/pokemon-trainer.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20220427%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220427T125740Z&X-Amz-Expires=86400&X-Amz-Signature=7b8f55a403e94aa914928f33efc3dd4ce2ac91d1d17cbee1a9a08216e039419e&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22pokemon-trainer.png%22&x-id=GetObject"
-                alt="profile"
-              />
+            </StyledLink>
+            <StyledLink to={"/profile"}>
+              <Icon src="/images/profile.png" alt="profile" />
               <IconDesc>프로필</IconDesc>
-            </IconBox>
-            <IconBox>
-              <Icon
-                src="https://s3.us-west-2.amazonaws.com/secure.notion-static.com/7cc5c00b-eff4-411b-bffa-56340d7d9caf/Untitled.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20220427%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220427T125925Z&X-Amz-Expires=86400&X-Amz-Signature=6d76c556a4f26a935ab1a2f9df55a8da3c2cab929acd37e58977b6f163c2e428&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22Untitled.png%22&x-id=GetObject"
-                alt="logout"
-              />
+            </StyledLink>
+            <StyledLink to={"/logout"}>
+              <Icon src="/images/logout.png" alt="logout" />
               <IconDesc>로그아웃</IconDesc>
-            </IconBox>
+            </StyledLink>
           </>
         ) : (
           <>
-            <IconBox>
-              <Icon
-                src="https://s3.us-west-2.amazonaws.com/secure.notion-static.com/329035de-a642-4cfa-93d3-6b815f644493/free-icon-pokeball-361998.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20220427%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220427T124807Z&X-Amz-Expires=86400&X-Amz-Signature=37e2d1550b54f8a256718488ce108b0a4057a03d76cdd87da63e31ffbf8773a7&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22free-icon-pokeball-361998.png%22&x-id=GetObject"
-                alt="pokedex"
-              />
+            <StyledLink to={"/pokedex"}>
+              <Icon src="/images/monsterball.png" alt="pokedex" />
               <IconDesc>도감</IconDesc>
-            </IconBox>
-            <IconBox>
-              <Icon
-                src="https://s3.us-west-2.amazonaws.com/secure.notion-static.com/98efa7f5-65a9-49aa-8753-8a335683678f/free-icon-pokemon-trainer-287277.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20220427%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220427T124735Z&X-Amz-Expires=86400&X-Amz-Signature=e4225b86b612166e67e7966a5f462394993e7dcf1d087a3f0e74446fe60e335f&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22free-icon-pokemon-trainer-287277.png%22&x-id=GetObject"
-                alt="login"
-              />
+            </StyledLink>
+            <StyledLink to={"/login"}>
+              <Icon src="/images/login.png" alt="login" />
               <IconDesc>로그인</IconDesc>
-            </IconBox>
+            </StyledLink>
           </>
         )}
       </Icons>
