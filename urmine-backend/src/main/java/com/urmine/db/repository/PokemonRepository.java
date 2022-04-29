aa3a5fa7710760c0750cd1a3377339990176ef6e
emost22 <emoney96@naver.com>
2022-04-29 10:17:59 +0900

    Feat: Create PokemonRepository
    DB에 접근하기 위해 PokemonRepository를 생성한다.

---
+++ urmine-backend/src/main/java/com/urmine/db/repository/PokemonRepository.java
@@ -0,0 +1,10 @@
+package com.urmine.db.repository;
+
+import com.urmine.db.entity.Pokemon;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PokemonRepository extends JpaRepository<Pokemon, Long> {
+
+}
