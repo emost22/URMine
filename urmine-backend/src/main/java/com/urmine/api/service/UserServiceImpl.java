87af4a48abea9daaf6a1b4f06dca88a8e6c75427
emost22 <emoney96@naver.com>
2022-05-10 19:14:12 +0900

    Feat: Add UserService variables
    + Kakao API 요청에 필요한 변수를 추가한다.

--- urmine-backend/src/main/java/com/urmine/api/service/UserServiceImpl.java
+++ urmine-backend/src/main/java/com/urmine/api/service/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.urmine.api.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 /*
@@ -7,5 +8,22 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class UserServiceImpl implements UserService {
-
+    @Value("${spring.security.oauth2.client.registration.kakao.client-id}")
+    private String kakaoClientId;
+    @Value("${spring.security.oauth2.client.registration.kakao.client-secret}")
+    private String kakaoClientSecret;
+    @Value("${spring.security.oauth2.client.registration.kakao.client-authentication-method}")
+    private String kakaoAuthenticationMethod;
+    @Value("${spring.security.oauth2.client.registration.kakao.authorization-grant-type}")
+    private String kakaoGrantType;
+    @Value("${spring.security.oauth2.client.registration.kakao.redirect-uri}")
+    private String kakaoRedirectUri;
+    @Value("${spring.security.oauth2.client.provider.kakao.authorization-uri}")
+    private String kakaoAuthorizationUri;
+    @Value("${spring.security.oauth2.client.provider.kakao.token-uri}")
+    private String kakaoTokenUri;
+    @Value("${spring.security.oauth2.client.provider.kakao.user-info-uri}")
+    private String kakaoUserInfoUri;
+    @Value("Bearer")
+    private String tokenType;
 }
