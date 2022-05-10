046dd0ca77bf096d85073e5d44d7529b9ddc0f09
emost22 <emoney96@naver.com>
2022-05-10 14:02:12 +0900

    Feat: Create UserService
    유저 정보 관리를 위한 Service를 생성한다.

---
+++ urmine-backend/src/main/java/com/urmine/api/service/UserServiceImpl.java
@@ -0,0 +1,11 @@
+package com.urmine.api.service;
+
+import org.springframework.stereotype.Service;
+
+/*
+ * 유저 정보 관리를 위한 ServiceImpl
+ */
+@Service
+public class UserServiceImpl implements UserService {
+
+}
