227378e546335f3e513682cb558a8ef60c286707
emost22 <emoney96@naver.com>
2022-05-12 10:16:44 +0900

    Feat: Create SwaggerConfig
    Swagger 사용을 위해 SwaggerConfiguration을 생성한다.

---
+++ urmine-backend/src/main/java/com/urmine/config/SwaggerConfig.java
@@ -0,0 +1,13 @@
+package com.urmine.config;
+
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/*
+ * Swagger 설정을 위한 SwaggerConfig
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    
+}
