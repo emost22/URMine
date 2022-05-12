c9e368f25b1e3228f77e472ef6ef9aa0f04a91f5
emost22 <emoney96@naver.com>
2022-05-12 11:16:54 +0900

    Feat: Add SwaggerConfig api Method
    패키지에 포함되는 클래스에 Swagger를 적용한다.

--- urmine-backend/src/main/java/com/urmine/config/SwaggerConfig.java
+++ urmine-backend/src/main/java/com/urmine/config/SwaggerConfig.java
@@ -1,12 +1,17 @@
 package com.urmine.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.ApiKey;
 import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.SecurityReference;
+import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.List;
@@ -22,6 +27,21 @@ public class SwaggerConfig {
     public static final String SECURITY_SCHEMA_NAME = "JWT";
     public static final String AUTHORIZATION_SCOPE_GLOBAL = "global";
     public static final String AUTHORIZATION_SCOPE_GLOBAL_DESC = "accessEverything";
+
+    @Bean
+    public Docket api() {
+        /*
+         * 해당 경로를 가지는 클래스에 Swagger를 적용하는 메소드
+         */
+        return new Docket(DocumentationType.OAS_30)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.urmine.api.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo())
+                .securitySchemes(newArrayList(apiKey()))
+                .securityContexts(newArrayList(securityContext()));
+    }
 
     private ApiKey apiKey() {
         /*
