7a3e7c9b17f728f1a398dd5b826230907f43279e
emost22 <emoney96@naver.com>
2022-05-13 17:36:39 +0900

    Fix: Remove unused pachage
    해당 클래스에서 사용하지 않는 패키지를 제거한다.

--- urmine-backend/src/main/java/com/urmine/api/service/UserServiceImpl.java
+++ urmine-backend/src/main/java/com/urmine/api/service/UserServiceImpl.java
@@ -7,8 +7,6 @@ import com.urmine.db.entity.User;
 import com.urmine.db.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.stereotype.Service;
 
 import java.io.BufferedReader;
