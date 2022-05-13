70ae9dac7d24b56cf1c44be6abef275922ab1113
emost22 <emoney96@naver.com>
2022-05-13 13:37:33 +0900

    Feat: Add UserRepository findUserByAccountEmail Method
    UserRepository에 accountEmail 기준으로 유저 정보를 조회하는 메소드를 추가한다.

--- urmine-backend/src/main/java/com/urmine/db/repository/UserRepository.java
+++ urmine-backend/src/main/java/com/urmine/db/repository/UserRepository.java
@@ -4,10 +4,15 @@ import com.urmine.db.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 /*
  * User 관련 query문 생성을 위한 JpaRepository
  */
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
-
+    /*
+     * accountEmail을 기준으로 유저를 검색하는 메소드
+     */
+    Optional<User> findUserByAccountEmail(String accountEmail);
 }
