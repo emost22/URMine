a16401c091999b840503e4f04de275deaf22735e
emost22 <emoney96@naver.com>
2022-05-18 15:08:46 +0900

    Feat: Add PokemonService findAllPokemon Method
    PokemonService에 모든 포켓몬 정보 조회 메소드를 생성한다.

--- urmine-backend/src/main/java/com/urmine/api/service/PokemonServiceImpl.java
+++ urmine-backend/src/main/java/com/urmine/api/service/PokemonServiceImpl.java
@@ -1,11 +1,31 @@
 package com.urmine.api.service;
 
+import com.urmine.api.response.pokemon.PokemonGetRes;
+import com.urmine.db.repository.PokemonEvolutionRepository;
+import com.urmine.db.repository.PokemonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /*
  * Pokemon 정보 관리를 위한 ServiceImpl
  */
 @Service
 public class PokemonServiceImpl implements PokemonService {
+    @Autowired
+    PokemonRepository pokemonRepository;
+    @Autowired
+    PokemonEvolutionRepository pokemonEvolutionRepository;
 
+    @Override
+    public List<PokemonGetRes> findAllPokemon() {
+        /*
+         * 모든 포켓몬의 정보를 조회하는 메소드
+         */
+
+        // TODO Add more
+
+        return null;
+    }
 }
